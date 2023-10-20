28e7422b1cc3c768fe9dfdc6b65953b1f7a446e2
unknown <hukhho@gmail.com>
2023-10-20 08:04:00 +0700

    oke

--- src/testnha/main.tsx
+++ src/testnha/main.tsx
@@ -2,13 +2,7 @@ import { MedusaProvider } from "medusa-react"
 import type { PropsWithChildren } from "react"
 import React from "react"
 import { createRoot } from "react-dom/client"
-import App from "./App"
-import "./assets/styles/global.css"
-import { LayeredModalProvider } from "./components/molecules/modal/layered-modal"
-import { SteppedProvider } from "./components/molecules/modal/stepped-modal"
-import { FeatureFlagProvider } from "./context/feature-flag"
-import { medusaUrl } from "./services/config"
-import queryClient from "./services/queryClient"
+import "@/assets/styles/global.css"
 
 const Page = ({ children }: PropsWithChildren) => {
   return (
