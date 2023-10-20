752a91784b9b9869abf341e5e42f484f50cb275f
unknown <hukhho@gmail.com>
2023-10-20 07:04:00 +0700

    oke

--- src/App.tsx
+++ src/App.tsx
@@ -29,15 +29,20 @@ const router = createBrowserRouter(
 )
 
 const Loading = () => (
-  <div className="flex w-full h-screen justify-center items-center bg-grey-5 text-grey-90">
+  <div className="flex h-screen w-full items-center justify-center bg-grey-5 text-grey-90">
     <Spinner variant="secondary" />
   </div>
 )
 
-const App = () => (
-  <Suspense fallback={<Loading />}>
-    <RouterProvider router={router} />
-  </Suspense>
-)
+const App = () => {
+  console.log("App.tsx, router: ", router)
+
+  return
+  {
+    <Suspense fallback={<Loading />}>
+      <RouterProvider router={router} />
+    </Suspense>
+  }
+}
 
 export default App
