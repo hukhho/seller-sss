4f0f6f5238c2d9fc1b61d3c6a27a1285201a8972
DESKTOP-SJS8V3V\ADMIN <hukhho@gmail.com>
2023-10-26 17:42:28 +0700

    c

--- src/services/config.ts
+++ src/services/config.ts
@@ -1,4 +1,4 @@
-// const medusaUrl = __MEDUSA_BACKEND_URL__ || "https://admin.sneakerss.net"
-const medusaUrl = __MEDUSA_BACKEND_URL__ || "http://localhost:9000"
+const medusaUrl = __MEDUSA_BACKEND_URL__ || "https://admin.sneakerss.net"
+// const medusaUrl = __MEDUSA_BACKEND_URL__ || "http://localhost:9000"
 
 export { medusaUrl }
